93610c96748a3316a06112889b61791d270fed69
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-10-17 09:25:33 -0700

    Create MyFirstVariable.java

---
+++ MyFirstVariable.java
@@ -0,0 +1,18 @@
+
+public class MyFirstVariable {
+
+	public static void main(String[] args) {
+		int big_int = 123;
+		double double_trouble = 34.34;
+		boolean one_two_punch = true;
+		char letter = 'a';
+		String feelings = "i dont like string";
+		
+		System.out.println("big_int : " + big_int);
+		System.out.println("double_trouble : " + double_trouble);
+		System.out.println("one_two_punch : " + one_two_punch);
+		System.out.println("letter : " + letter);
+		System.out.println("feelings : " + feelings);
+	}
+
+}

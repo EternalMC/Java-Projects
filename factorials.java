7f6fbc27e50d1bdd10a63e4b34c1ed323f75c109
EternalMC <55027815+EternalMC@users.noreply.github.com>
2019-12-12 11:31:10 -0600

    Create factorials.java

---
+++ factorials.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Factorials {
+
+public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+int user_int = input.nextInt();
+
+Scanner.hasNextInt();
+
+if(user_int) {
+System.out.println("Try again as a integer!");
+input.close();
+return;
+}
+long i;
+long factorial;
+for(factorial = factorial *i; ) {
+
+}
+  }
+}
+
